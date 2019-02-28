78ea1b9b5d7fba1e3705dfcc3b28345bafa7d0bf
Kin Chan <xkinchanx@gmail.com>
2019-02-28 15:57:10 -0800

    [setup] skeleton

---
+++ client/src/index.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import { render } from 'react-dom';
+import BoardService from './components/BoardService';
+
+render(<BoardService />, document.getElementById('boardService'));

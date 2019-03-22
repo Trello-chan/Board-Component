ad75443841909873a3f42692ceddb0ffb6af4c0c
Kin Chan <xkinchanx@gmail.com>
2019-03-22 11:07:19 -0700

    [prog] establishing context for dnd

--- client/src/components/BoardService.js
+++ client/src/components/BoardService.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { DragDropContext } from 'react-beautiful-dnd';
 
 import Column from './Column';
 
@@ -13,15 +14,25 @@ class BoardService extends Component {
     }
   }
 
+  onDragEnd = (result) => {
+
+  }
+
   render() {
     let { lists } = this.state;
     return (
-      <div>
-        hello
-        {lists.map((list, index) => 
-          <Column key={list.id} list={list} />
-        )}
-      </div>
+      <DragDropContext
+        // onDragStart
+        // onDragUpdate
+        onDragEnd={this.onDragEnd}
+      >
+        <div>
+          hello
+          {lists.map((list, index) => 
+            <Column key={list.id} list={list} />
+          )}
+        </div>
+      </DragDropContext>
     )
   }
 }

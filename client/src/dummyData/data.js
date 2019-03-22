1082173334ea6657a5b0ebdc881fff68ff3a9f4a
Kin Chan <xkinchanx@gmail.com>
2019-03-22 11:08:18 -0700

    [prog] added id to cards for dnd

--- client/src/dummyData/data.js
+++ client/src/dummyData/data.js
@@ -1,3 +1,95 @@
+const cards1 = [
+  {
+    id: 10,
+    label: 'Board Component: drag/drop 1',
+    description: 'Board Component: drag/drop',
+    comment: [],
+    list_id: 1,
+    board_id: 1
+  },
+  {
+    id: 11,
+    label: 'Board Component: drag/drop 1',
+    description: 'Board Component: drag/drop',
+    comment: [],
+    list_id: 1,
+    board_id: 1
+  },
+  {
+    id: 12,
+    label: 'Board Component: drag/drop 1',
+    description: 'Board Component: drag/drop',
+    comment: [],
+    list_id: 1,
+    board_id: 1
+  },
+  {
+    id: 13,
+    label: 'Board Component: drag/drop 1',
+    description: 'Board Component: drag/drop',
+    comment: [],
+    list_id: 1,
+    board_id: 1
+  },
+];
+
+const cards2 = [
+  {
+    id: 14,
+    label: 'Board Component: drag/drop 2',
+    description: 'Board Component: drag/drop',
+    comment: [],
+    list_id: 2,
+    board_id: 1
+  },
+  {
+    id: 15,
+    label: 'Board Component: drag/drop 2',
+    description: 'Board Component: drag/drop',
+    comment: [],
+    list_id: 2,
+    board_id: 1
+  },
+  {
+    id: 16,
+    label: 'Board Component: drag/drop 2',
+    description: 'Board Component: drag/drop',
+    comment: [],
+    list_id: 2,
+    board_id: 1
+  },
+]
+
+const cards3 = [
+  {
+    id: 17,
+    label: 'Board Component: drag/drop 3',
+    description: 'Board Component: drag/drop',
+    comment: [],
+    list_id: 3,
+    board_id: 1
+  },
+]
+
+const cards4 = [
+  {
+    id: 18,
+    label: 'Board Component: drag/drop 4',
+    description: 'Board Component: drag/drop',
+    comment: [],
+    list_id: 4,
+    board_id: 1
+  },
+  {
+    id: 19,
+    label: 'Board Component: drag/drop 4',
+    description: 'Board Component: drag/drop',
+    comment: [],
+    list_id: 4,
+    board_id: 1
+  },
+];
+
 const lists = [
   {
     id: 1,
@@ -20,87 +112,5 @@ const lists = [
     cards: cards4
   },
 ];
-
-const cards1 = [
-  {
-    label: 'Board Component: drag/drop 1',
-    description: 'Board Component: drag/drop',
-    comment: [],
-    list_id: 1,
-    board_id: 1
-  },
-  {
-    label: 'Board Component: drag/drop 1',
-    description: 'Board Component: drag/drop',
-    comment: [],
-    list_id: 1,
-    board_id: 1
-  },
-  {
-    label: 'Board Component: drag/drop 1',
-    description: 'Board Component: drag/drop',
-    comment: [],
-    list_id: 1,
-    board_id: 1
-  },
-  {
-    label: 'Board Component: drag/drop 1',
-    description: 'Board Component: drag/drop',
-    comment: [],
-    list_id: 1,
-    board_id: 1
-  },
-];
-
-const cards2 = [
-  {
-    label: 'Board Component: drag/drop 2',
-    description: 'Board Component: drag/drop',
-    comment: [],
-    list_id: 2,
-    board_id: 1
-  },
-  {
-    label: 'Board Component: drag/drop 2',
-    description: 'Board Component: drag/drop',
-    comment: [],
-    list_id: 2,
-    board_id: 1
-  },
-  {
-    label: 'Board Component: drag/drop 2',
-    description: 'Board Component: drag/drop',
-    comment: [],
-    list_id: 2,
-    board_id: 1
-  },
-]
-
-const cards3 = [
-  {
-    label: 'Board Component: drag/drop 3',
-    description: 'Board Component: drag/drop',
-    comment: [],
-    list_id: 3,
-    board_id: 1
-  },
-]
-
-const cards4 = [
-  {
-    label: 'Board Component: drag/drop 4',
-    description: 'Board Component: drag/drop',
-    comment: [],
-    list_id: 4,
-    board_id: 1
-  },
-  {
-    label: 'Board Component: drag/drop 4',
-    description: 'Board Component: drag/drop',
-    comment: [],
-    list_id: 4,
-    board_id: 1
-  },
-]
 
 export default lists;

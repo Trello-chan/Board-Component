06d7d9289f8c4487e071ea4b34bc64533d236a99
Kin Chan <xkinchanx@gmail.com>
2019-03-22 11:07:35 -0700

    [prog] droppable container

--- client/src/components/Column.js
+++ client/src/components/Column.js
@@ -1,4 +1,7 @@
 import React, { Component } from 'react';
+import { Droppable } from 'react-beautiful-dnd';
+
+import Card from './Card';
 
 class Column extends Component {
   constructor(props) {
@@ -10,10 +13,38 @@ class Column extends Component {
 
   render() {
     let { list } = this.props;
+    console.log(list)
     return(
-      <div>{list.name}</div>
+      <Container>
+        <Title>{list.name}</Title>
+        <Droppable droppableId={list.id}>
+          {provided =>
+            <BoardList
+              ref={provided.innerRef}
+              {...provided.droppableProps}
+            >
+              {list.cards.map((card, index) => <Card key={index} index={index} card={card}/>)}
+              {provided.placeholder}
+            </BoardList>
+          }
+        </Droppable>
+      </Container>
     )
   }
 }
+
+const Container = styled.div`
+  margin: 8px;
+  border: 1px solid lightgrey;
+  border-radius: 2px;
+`;
+
+const Title = styled.h3`
+  padding: 8px;
+`;
+
+const BoardList = styled.div`
+padding: 8px;
+`;
 
 export default Column;

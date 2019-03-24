5a432a56fc6601fbb62490549d1ee02d5970ea80
Kin Chan <xkinchanx@gmail.com>
2019-03-24 00:44:39 -0700

    [prog] schema design based on expected queries

---
+++ database/schema.js
@@ -0,0 +1,46 @@
+import Sequelize from 'sequelize';
+import SQL_connection from './index';
+
+const Member = SQL_connection.define('member', {
+  name: Sequelize.STRING
+});
+
+const Team = SQL_connection.define('team', {
+  teamname: Sequelize.STRING //don't really need other fields for a mock module
+});
+
+const Board = SQL_connection.define('board', {
+  title: Sequelize.STRING,
+  team_id: { type: Sequelize.INTEGER, allowNull: true, references: { model: 'teams', key: 'id' }, onUpdate: 'CASCADE', onDelete: 'CASCADE'},
+  background_image: { type: Sequelize.STRING, allowNull: true }
+});
+
+const List = SQL_connection.define('list', {
+  name: Sequelize.STRING,
+  board_id: { type: Sequelize.INTEGER, references: { model: 'boards', key: 'id' }, onUpdate: 'CASCADE', onDelete: 'CASCADE'}
+
+})
+
+const Card = SQL_connection.define('card', {
+  label: Sequelize.STRING,
+  description: Sequelize.STRING,
+  comment: Sequelize.STRING,
+  list_index: { type: Sequelize.INTEGER, allowNull: false },
+  list_id: { type: Sequelize.INTEGER, references: { model: 'lists', key: 'id' }, onUpdate: 'CASCADE', onDelete: 'CASCADE'}
+});
+
+const Card_Member = SQL_connection.define('card_member', {});
+
+Card.belongsToMany(Member, { through: 'card_members', foreignKey: 'card_id' });
+Member.belongsToMany(Card, { through: 'card_members', foreignKey: 'member_id' });
+
+SQL_connection.sync();
+
+export {
+  Board,
+  Card,
+  Card_Member,
+  List,
+  Member,
+  Team
+}

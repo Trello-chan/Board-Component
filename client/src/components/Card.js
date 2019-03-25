279d46b9d0c0660c075e9a4762f3f01de76f2d61
Kin Chan <xkinchanx@gmail.com>
2019-03-25 11:14:53 -0700

    [prog] settng state to show Card overlay, using react 16's new useState feature for funs

--- client/src/components/Card.js
+++ client/src/components/Card.js
@@ -1,8 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Draggable } from 'react-beautiful-dnd';
 
-const Card = ({ card, index }) =>
-  <Draggable draggableId={`card.${card.id}`} index={index}>
+import CardModal from './Card/Card';
+
+const Card = ({ card, index }) => {
+  const [showOverlay, setOverlay] = useState(false);
+  
+  return (<Draggable draggableId={`card.${card.id}`} index={index}>
     {(provided, snapshot) => 
       <Container
         {...provided.draggableProps}
@@ -11,18 +15,24 @@ const Card = ({ card, index }) =>
         isDragging={snapshot.isDragging}
         draggingOver={snapshot.draggingOver}
       >
-      {card.label}
+        <div onClick={() => setOverlay(true)}>{card.label}</div>
+        {showOverlay && <CardModal card={card} handleCardClick={() => setOverlay(false)}/>}
       </Container>
     }
-  </Draggable>
+  </Draggable>)
+}
 
 const Container = styled.div`
   border: 1px solid lightgrey;
-  border-radius: 2px;
+  border-radius: 3px;
+  border-bottom: 1px solid grey;
   box-shadow: ${props => props.isDragging ? '5px 5px 3px grey' : '0px 0px'};
   padding: 8px;
   margin-bottom: 8px;
   background-color: ${({isDragging, draggingOver}) => isDragging && !draggingOver ? 'rgba(100,100,100,0.5)' : 'white'};
+  &:hover{
+    cursor: pointer;
+  }
 `;
 
 export default Card;

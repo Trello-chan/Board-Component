a599ea6e8064e36e431fd2111405cbf0975675fd
Kin Chan <xkinchanx@gmail.com>
2019-03-24 22:43:34 -0700

    [style] lists to look like trello

--- client/src/components/Column.js
+++ client/src/components/Column.js
@@ -37,32 +37,43 @@ class Column extends Component {
             </Droppable>
           </Container>
         }
+
+
       </Draggable>
     )
   }
 }
 
 const Container = styled.div`
-  margin: 8px;
+  color: #17394d;
+  font-family: Helvetica Neue,Arial,Helvetica,sans-serif;
+  font-size: 14px;
+  margin: 4px;
   border: 1px solid lightgrey;
-  border-radius: 2px;
+  border-radius: 3px;
   display: flex;
   flex-direction: column;
   max-width: 275px;
   min-width: 275px;
-  background-color: white;
+  background-color: #dfe3e6;
+  height: auto;
 `;
 
-const Title = styled.h3`
+const Title = styled.div`
   padding: 8px;
+  font-weight: 700;
 `;
 
 const BoardList = styled.div`
-  background-color: ${props => props.isDraggingOver ? 'rgba(100,100,100,0.5)' : 'white'};
+  background-color: ${props => props.isDraggingOver ? 'rgba(100,100,100,0.5)' : '#dfe3e6'};
   padding: 8px;
   flex-grow: 1;
   max-height: 100%;
   overflow: scroll;
 `;
+
+const BlankSpace = styled.div`
+  height: auto;
+`
 
 export default Column;
